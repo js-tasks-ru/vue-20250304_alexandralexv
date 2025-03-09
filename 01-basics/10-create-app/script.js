e1a5dc538d4c97d5e4eee4e22119ab7a410c17e6
Alex Semionov <alexandralexv@gmail.com>
2025-03-09 15:25:49 +0300

    01-basics/10-create-app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,23 @@
 import { defineComponent, createApp } from 'vue'
+
+
+
+const App = defineComponent({
+  name: 'App',
+
+  setup() {
+    const DATE = new Date().toLocaleDateString(navigator.language, { dateStyle: 'long' })
+    
+    return {
+      DATE,
+    }
+  },
+
+  template: `<div>Сегодня {{ DATE }}</div>`,
+})
+
+const app = createApp(App)
+
+const vm = app.mount('#app')
+
+window.vm = vm

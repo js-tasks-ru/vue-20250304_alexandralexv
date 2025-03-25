5382a280d452f8ebcdaaea39eb9b4fcfe618a4e2
Alex Semionov <alexandralexv@gmail.com>
2025-03-25 18:15:37 +0300

    05-ts/10-MeetupView-TS

--- 05-ts/10-MeetupView-TS/MeetupCover.vue
+++ 05-ts/10-MeetupView-TS/MeetupCover.vue
@@ -1,18 +1,15 @@
-<script setup>
+<script setup lang="ts">
 import { computed } from 'vue'
+import type { StyleValue } from 'vue'
 
-const props = defineProps({
-  title: {
-    type: String,
-    required: true,
-  },
+const props = defineProps<{
+  title: string
+  image?: string
+}>()
 
-  image: {
-    type: String,
-  },
-})
-
-const bgStyle = computed(() => (props.image ? { '--bg-url': `url('${props.image}')` } : undefined))
+const bgStyle = computed<StyleValue | undefined>(() =>
+  props.image ? { '--bg-url': `url('${props.image}')` } : undefined,
+)
 </script>
 
 <template>
